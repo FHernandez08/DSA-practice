101eced1a3e275bd88fec9c89fd07cbc32885f2a
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-02-27 13:39:12 -0600

    mixed problem - hashing and set

---
+++ Week 1/Mixed Practice/common_words.py
@@ -0,0 +1,4 @@
+# Common Words in Two Dictionaries (Dictionaries & Sets)
+# You are given two dictionaries (dict1 & dict2), where the keys are words, and the values represent their frequencies.
+# Write a function to return a set of words that appear in both dictionaries at least once.
+
---
+++ Week 1/Mixed Practice/rotated_search.py
@@ -0,0 +1,3 @@
+# Search in Rotated Sorted Array (Binary Search)
+# Given a rotated sorted array (nums) of distinct integers and an integer (target). Implement a function to return the index of target
+# if found, otherwise return -1. Must run in O (log n) time complexity.
