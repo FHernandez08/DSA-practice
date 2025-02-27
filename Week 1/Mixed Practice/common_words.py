cf8ab4492376138ff6fa33cc1bdc57834ebaf3c5
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-02-27 13:55:50 -0600

    Completed the mixed practice for week 1

--- Week 1/Mixed Practice/common_words.py
+++ Week 1/Mixed Practice/common_words.py
@@ -2,3 +2,10 @@
 # You are given two dictionaries (dict1 & dict2), where the keys are words, and the values represent their frequencies.
 # Write a function to return a set of words that appear in both dictionaries at least once.
 
+def common_dict(dict_1, dict_2):
+    return set(dict_1.keys()) & set(dict_2.keys())
+
+dict1 = {"apple": 3, "banana": 5, "cherry": 2, "date": 1}
+dict2 = {"banana": 4, "cherry": 1, "fig": 2, "grape": 3}
+
+print(common_dict(dict1, dict2))
--- Week 1/Mixed Practice/rotated_search.py
+++ Week 1/Mixed Practice/rotated_search.py
@@ -1,3 +1,31 @@
 # Search in Rotated Sorted Array (Binary Search)
 # Given a rotated sorted array (nums) of distinct integers and an integer (target). Implement a function to return the index of target
 # if found, otherwise return -1. Must run in O (log n) time complexity.
+
+def sorted_array(nums, target):
+    low = 0
+    high = len(nums) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if nums[mid] == target:
+            return mid
+        
+        if nums[low] <= nums[mid]:
+            if nums[low] <= target < nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:
+            if nums[mid] <= target < nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+    
+    return -1
+
+
+nums = [4, 5, 6, 7, 0, 1, 2]
+target = 0
+print(sorted_array(nums, target))
